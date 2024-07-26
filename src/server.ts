207da43b0e8addc70cdd6b4823e12f915a631b4c
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2024-07-26 16:57:28 -0400

    feat: create basic fastify route

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('hello world')
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/', (res, req) => {
+  return 'Hello World'
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('🚀 Server running on port 3333.')
+})
