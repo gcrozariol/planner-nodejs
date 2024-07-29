90e4184316711f9af1c0cefb51c250773b2eb10d
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2024-07-29 19:18:32 -0400

    chore: change email language

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -85,15 +85,15 @@ export async function createTrip(app: FastifyInstance) {
         subject: 'Send test email',
         html: `
           <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
-          <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
+          <p>You requested a trip to <strong>${destination}</strong> from <strong>${formattedStartDate}</strong> to <strong>${formattedEndDate}</strong>.</p>
           <p></p>
-          <p>Para confirmar sua viagem, clique no link abaixo:</p>
+          <p>To confirm your trip, click the link below:</p>
           <p></p>
           <p>
-            <a href="${confirmationLink}">Confirmar viagem</a>
+            <a href="${confirmationLink}">Confirm trip</a>
           </p>
           <p></p>
-          <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
+          <p>If you don't know what this email is about, just ignore this email.</p>
         </div>
         `.trim(),
       })
